2c4208d1537d10efa96d3bec6b686d82f2d8cf72
Jaydip Dandwate <jaydipdandwate2283@gmail.com>
2025-07-31 16:33:44 +0530

    Add home page for Render deployment

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 # movies/urls.py
 from django.urls import path
-from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView
+from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView,home
 
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-retrieve-update-destroy'),
+    path('', home),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,11 @@
-# movies/views.py
 from rest_framework import generics
 from .models import Movie
 from .serializers import MovieSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Django Movie API is running successfully 🚀")
+
 
 class MovieListCreateView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
