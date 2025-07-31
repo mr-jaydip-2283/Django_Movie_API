bb7cd9e695a32eb130b5e2fffe051f742ffa45b5
Jaydip Dandwate <jaydipdandwate2283@gmail.com>
2025-07-31 16:43:35 +0530

    Add homepage for Render deployment

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,5 @@ from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView,home
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-retrieve-update-destroy'),
-    path('', home),
+    path('', home, name='home'),  # Home view
 ]
